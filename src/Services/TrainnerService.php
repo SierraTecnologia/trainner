ade10370e8f0c4fe78b103185876469fd72ad177
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-01-04 06:41:33 -0300

    Ajeitando Configuracoes e outros erros

--- src/Services/TrainnerService.php
+++ src/Services/TrainnerService.php
@@ -18,7 +18,7 @@ class TrainnerService
     public function __construct($config = false)
     {
         if (!$this->config = $config) {
-            $this->config = config('facilitador.sitec.models');
+            $this->config = config('sitec.sitec.models');
         }
     }
 
