0cc7914bf30aad35219e5f3b80a71bd236157c61
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-25 11:21:31 -0300

    Template do PEdreiro para Todos, ajeitando problemas

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
         $this->service = $service;
     }
 
-    public function index()
+    public function index(Request $request)
     {
         return view('trainner::dash.home');
     }
